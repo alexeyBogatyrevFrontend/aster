641a3f520df6a8f076728c9fdb0abd06b366c857
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-08-01 09:12:12 +0500

    finish project

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,9 @@
 const burger = document.querySelector('#burger')
 const menu = document.querySelector('#menu')
 
+// handler
 burger.addEventListener('click', () => {
+	// change class
 	burger.classList.toggle('active')
 	menu.classList.toggle('active')
 })
@@ -14,9 +16,12 @@ accordions.forEach(accordion => {
 	const top = accordion.querySelector('.faq__accordion-top')
 	const content = accordion.querySelector('.faq__accordion-content')
 
+	// handler
 	top.addEventListener('click', () => {
+		// change class
 		accordion.classList.toggle('open')
 
+		// сhange the height depending on the content
 		if (accordion.classList.contains('open')) {
 			content.style.maxHeight = content.scrollHeight + 'px'
 		} else {
