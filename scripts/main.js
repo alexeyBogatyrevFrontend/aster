4299c32344dbeca9443e4cd005dcb0e24525f14d
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-08-01 08:17:44 +0500

    add faq section

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
+// burger menu
 const burger = document.querySelector('#burger')
 const menu = document.querySelector('#menu')
 
 burger.addEventListener('click', () => {
 	burger.classList.toggle('active')
 	menu.classList.toggle('active')
+})
+
+// accordion
+const accordions = document.querySelectorAll('.faq__accordion')
+
+accordions.forEach(accordion => {
+	const top = accordion.querySelector('.faq__accordion-top')
+	const content = accordion.querySelector('.faq__accordion-content')
+
+	top.addEventListener('click', () => {
+		accordion.classList.toggle('open')
+
+		if (accordion.classList.contains('open')) {
+			content.style.maxHeight = content.scrollHeight + 'px'
+		} else {
+			content.style.maxHeight = null
+		}
+	})
 })
